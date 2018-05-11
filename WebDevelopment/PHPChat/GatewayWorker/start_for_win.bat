16dfbc842a6fbf2affd3e82f938c5c13da557501
scottdu <dytscott@gmail.com>
2018-05-11 09:54:04 +0800

    remove PHPchat to miniproject

--- WebDevelopment/PHPChat/GatewayWorker/start_for_win.bat
+++
@@ -1,2 +0,0 @@
-php Applications\YourApp\start_register.php  Applications\YourApp\start_gateway.php Applications\YourApp\start_businessworker.php
-pause
--- WebDevelopment/PHPChat/conn.php
+++
@@ -1,3 +0,0 @@
-<?php
-$link = mysqli_connect('localhost', 'duyuntao', '', 'mysql');
-?>  
