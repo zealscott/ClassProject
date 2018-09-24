fbab95dce16be7722bc4c0e4ace5db028b13eb6d
scottdu <dytscott@gmail.com>
2018-09-24 19:33:37 +0800

    mongoDB in java

--- SoftwareEngineering/Project1/str_convert.cpp
+++ SoftwareEngineering/Project1/str_convert.cpp
@@ -7,21 +7,24 @@
 
 #include <string.h>
 
+// add int32 format
+#include <stdint.h>
+
 #include <math.h>
 
 #include <string>
 
-namespace std {
+
 // convert string to integer
 // if cannot convert, return 0
-int str_to_int32(string str) {
-  int val = atoi(str.c_str());
+int32_t  str_to_int32(std::string str) {
+  int32_t  val = (int32_t)atoi(str.c_str());
   return val;
 }
 
 // convert string to double
 // if cannot convert, return 0
-double str_to_double(string str) {
+double str_to_double(std::string str) {
   double integer = 0;  // integer part of double number 
   double decimal = 0;  // decimal part of double number
   int str_len = (int)strlen(str.c_str());
@@ -46,17 +49,17 @@ double str_to_double(string str) {
 
   return integer + decimal / pow(10, str_len - point - 1);
 }
-} // namespace std
+
 
 int main() {
   std::string str;
   str.resize(100);
   scanf("%s", &str[0]);
 
-  double val = str_to_double(str);
-  int val_int = str_to_int32(str);
+  double val_double = str_to_double(str);
+  int32_t  val_int32 = str_to_int32(str);
   
-  printf("convert to double: %f\n",val);
-  printf("convert to int32: %d\n",val_int);
+  printf("convert to double: %f\n",val_double);
+  printf("convert to int32: %d\n",val_int32);
   return 0;
 }
